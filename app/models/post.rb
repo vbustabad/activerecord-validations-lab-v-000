5f9add3e6d61fb2acff9ca91771eb590aa5071a3
Veronica Bustabad <vbustabad@gmail.com>
2018-08-07 23:56:06 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,9 @@ class Post < ActiveRecord::Base
   validate :is_the_title_clickbait?
 
   def is_the_title_clickbait?
-  clickbait_criteria = ["Won't Believe", "Secret", "Top[0..10]", "Guess"]
+  clickbait_content = ["Won't Believe", "Secret", "Top[0..10]", "Guess"]
 
-    if clickbait_criteria.include?(title)
+    if clickbait_content.include?(title)
       true
     else
       errors.add(:title, "The title must be clickbait.")
