c9c44a6b2795e653df083f108ec117b1ffd94f70
Veronica Bustabad <vbustabad@gmail.com>
2018-08-07 23:12:06 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
-    message: "%{value} is not a valid category" }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :is_clickbait?, inclusion: { in: %w(Won't Believe Secret Top[number] Guess) }
 end
