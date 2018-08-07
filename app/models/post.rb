455c57fc45651ec2d28d1797a3937dc46ecc645e
Veronica Bustabad <vbustabad@gmail.com>
2018-08-07 23:26:00 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,16 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validates :clickbait, inclusion: { in: %w(Won't Believe Secret Top[number] Guess) }
+  validates :is_the_title_clickbait?
+
+  clickbait_content = ["Won't Believe", "Secret", "Top[0-10]", "Guess"]
+
+  def is_the_title_clickbait?
+    if clickbait_content.includes?(title)
+      true
+    else
+      errors.add(:title, "The title must be clickbait.")
+    end
+  end
+
 end
