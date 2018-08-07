79e5722884c8080dcd2cc206fa3c13a1b5f3a29c
Veronica Bustabad <vbustabad@gmail.com>
2018-08-07 22:30:26 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
-    message: "%{value} is not a valid size" }
+    message: "%{value} is not a valid category" }
 end
